98466edd495408aeff53780fbba2d904525a6be9
mzlittle <zach@rightscale.com>
2012-01-26 19:00:14 +0000

    fixed

--- cookbooks/hello_world/metadata.rb
+++ cookbooks/hello_world/metadata.rb
@@ -1,6 +1,8 @@
-maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
-license          "All rights reserved"
-description      "Installs/Configures hello_world"
+maintainer "RightScale"
+maintainer_email "zach@rightscale.com"
+license "All rights reserved"
+description "Installs/Configures hello_world"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.1"
+version "0.0.1"
+
+recipe "hello_world::default", "Prints to the audit entry"
